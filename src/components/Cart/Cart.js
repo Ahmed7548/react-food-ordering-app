c2e3c1e909d244e8fafe68ee5cc41614fd16d4a5
Ahmed Mamdouh <tatah.douh.vom@gmail.com>
2021-11-07 12:32:39 +0200

    managing checkout form

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,45 +1,48 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 
-import classes from "./Cart.module.css"
+import classes from "./Cart.module.css";
 import CartItem from "./CartItem";
 import CartContext from "../../store/cart-context";
 import CheckOut from "./CheckOut";
 import { useState } from "react/cjs/react.development";
 
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const [displayCheckOut, setDisplayCheckOut] = useState(false);
 
+  const cartItems = (
+    <ul style={{ padding: 0 }}>
+      {cartCtx.items.map((item) => (
+        <CartItem item={item} key={item.id} />
+      ))}
+    </ul>
+  );
 
-const Cart = props => {
-    const cartCtx = useContext(CartContext)
-    const cartItems = <ul style={{ padding: 0 }}>{cartCtx.items.map(item => <CartItem item={item} key={item.id}/>)}</ul>
-    const [disabled,setDisabled]=useState(false)
-    const submitHandle = (event) => {
-        event.preventDefault()
-    }
-   
-    const changeHandle = (disabled) => {
-        setDisabled(disabled)
-    }
-  
+  const clickHandle = () => {
+    setDisplayCheckOut(true);
+  };
+  const Buttons = (
+    <div className={classes.buttons}>
+      <button className={classes.order} onClick={clickHandle}>
+        order
+      </button>
+      <button className={classes.close} onClick={props.onClick}>
+        close
+      </button>
+    </div>
+  );
 
-    return (
-        <div className={classes.cart}>
-            {cartItems}
-            <div className={classes["total"]}>
-                <span>Total amount</span>
-                <span>${cartCtx.totalAmount.toFixed(2)}</span>
-            </div>
-            <CheckOut onSubmit={submitHandle} onChange={changeHandle}>
-            <div className={classes.buttons}>
-                <button className={classes.order} type="submit" disabled={!disabled}>
-                    order
-                </button>
-                <button className={classes.close} onClick={props.onClick} >
-                    close
-                </button>
-            </div>
-            </CheckOut>
-        </div>
-    )
-}
+  return (
+    <div className={classes.cart}>
+      {cartItems}
+      <div className={classes["total"]}>
+        <span>Total amount</span>
+        <span>${cartCtx.totalAmount.toFixed(2)}</span>
+      </div>
+      {displayCheckOut && <CheckOut onClose={props.onClick} />}
+      {!displayCheckOut && Buttons}
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
--- src/components/Cart/CheckOut.js
+++ src/components/Cart/CheckOut.js
@@ -1,111 +1,164 @@
-import { useState,useReducer } from "react";
-import { useEffect } from "react/cjs/react.development";
-import Input from "../UI/input/Input";
-import classes from "./CheckOut.module.css"
+import { useReducer } from "react";
+
+import classes from "./CheckOut.module.css";
 const initialValue = {
-    name: { value: "", isValid: false },
-    adress: { value: "", isValid: false },
-    email: { value: "", isValid: false }
-}
+  name: { value: "", isValid: false },
+  street: { value: "", isValid: false },
+  postalCode: { value: "", isValid: false },
+  city: { value: "", isValid: false },
+};
 
 const reducerFunction = (state, action) => {
-    if (action.type === "EMCHANGE") {
-        if (action.value.includes("@")) {
-            return {
-                name: state.name,
-                adress:state.adress,
-                email:{ value: action.value, isValid: true }
-            }
-        } else {
-            return {
-                name: state.name,
-                adress:state.adress,
-                email:{ value: action.value, isValid: false }
-            }
-        }
+  if (action.type === "STCHANGE") {
+    if (action.value.trim() !== "") {
+      return {
+        name: state.name,
+        street: { value: action.value, isValid: true },
+        postalCode: state.postalCode,
+        city: state.city,
+      };
+    } else {
+      return {
+        name: state.name,
+        street: { value: action.value, isValid: false },
+        postalCode: state.postalCode,
+        city: state.city,
+      };
     }
-    if (action.type === "NACHANGE") {
-        if (action.value.trim()!=="") {
-            return {
-                name: {value:action.value, isValid:true},
-                adress:state.adress,
-                email:state.email
-            }
-        } else {
-            return {
-                name: { value: action.value, isValid: false },
-                adress:state.adress,
-                email:state.email
-            }
-        }
+  }
+  if (action.type === "NACHANGE") {
+    if (action.value.trim() !== "") {
+      return {
+        name: { value: action.value, isValid: true },
+        street: state.street,
+        postalCode: state.postalCode,
+        city: state.city,
+      };
+    } else {
+      return {
+        name: { value: action.value, isValid: false },
+        street: state.street,
+        postalCode: state.postalCode,
+        city: state.city,
+      };
     }
-    if (action.type === "ADCHANGE") {
-        if (action.value.trim()!=="") {
-            return {
-                name:state.name, 
-                adress:{value:action.value, isValid:true},
-                email:state.email
-            }
-        } else {
-            return {
-                name: state.name,
-                adress:{ value: action.value, isValid: false },
-                email:state.adress
-            }
-        }
+  }
+  if (action.type === "CTCHANGE") {
+    if (action.value.trim() !== "") {
+      return {
+        name: state.name,
+        street: state.street,
+        postalCode: state.postalCode,
+        city: { value: action.value, isValid: true },
+      };
+    } else {
+      return {
+        name: state.name,
+        street: state.street,
+        postalCode: state.postalCode,
+        city: { value: action.value, isValid: false },
+      };
     }
+  }
+  if (action.type === "PCCHANGE") {
+    const value = Number(action.value);
+    if (!isNaN(value) && action.value.length === 4) {
+      return {
+        name: state.name,
+        street: state.street,
+        postalCode: { value: action.value, isValid: true },
+        city: state.city,
+      };
+    } else {
+      return {
+        name: state.name,
+        street: state.street,
+        postalCode: { value: action.value, isValid: false },
+        city: state.city,
+      };
+    }
+  }
 
-    return initialValue
-}
+  return initialValue;
+};
 const CheckOut = (props) => {
-    // const [Name, setName] = useState("")
-    // const [adress, setAdress] = useState("")
-    // const [email, setEmail] = useState("")
+  const submitHandle = (event) => {
+    event.preventDefault();
+  };
+  const [inputStates, dispatchInputStates] = useReducer(
+    reducerFunction,
+    initialValue
+  );
 
-    const [inputStates,dispatchInputStates]= useReducer(reducerFunction,initialValue)
-    
-    const emailChangeHandler = (value) => {
-        dispatchInputStates({ type: "EMCHANGE", value: value })
-        
-    }
-    const nameChangeHandler = (value) => {
-        dispatchInputStates({ type: "NACHANGE", value: value })
-        
-    }
-    const adressChangeHandler = (value) => {
-        dispatchInputStates({ type: "ADCHANGE", value: value })
-    }
-    
-    const{name,adress,email}=inputStates
-    
-    const nameValue=name.value
-    const emailValue=email.value
-    const adressValue = adress.value
-    let disabled = name.isValid && email.isValid && adress.isValid
-    useEffect(() => {
-        props.onChange(disabled)
-    },[disabled,props])
-    console.log(inputStates.name.value)
-    return (
-        <form className={classes.checkOut} onSubmit={props.onSubmit} >
-            <Input input={{
-                type: "emil",
-                id: "Email",
-                value: emailValue
-            }} label="Enter you email:" onChange={emailChangeHandler}/>
-            <Input input={{
-                type: "text",
-                id: "name",
-                value: nameValue
-            }} label="Name:" onChange={nameChangeHandler}/>
-             <Input input={{
-                type: "text",
-                id: "adress",
-                value:adressValue
-            }} label="Adress:" onChange={adressChangeHandler}/>
-            {props.children}
-        </form>
-    )
-}
+  const nameHandle = (event) => {
+    dispatchInputStates({ type: "NACHANGE", value: event.target.value });
+  };
 
-export default CheckOut
+  const streetHandle = (event) => {
+    dispatchInputStates({ type: "STCHANGE", value: event.target.value });
+  };
+
+  const postalCodeHandle = (event) => {
+    dispatchInputStates({ type: "PCCHANGE", value: event.target.value });
+  };
+
+  const cityHandle = (event) => {
+    dispatchInputStates({ type: "CTCHANGE", value: event.target.value });
+  };
+  const { name, street, postalCode, city } = inputStates;
+  console.log(postalCode.isValid);
+
+  return (
+    <form className={classes.checkOut} onSubmit={submitHandle}>
+      <div>
+        {" "}
+        <label htmlFor="name">Your name:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={name.value}
+          onChange={nameHandle}
+        ></input>
+      </div>
+      <div>
+        {" "}
+        <label htmlFor="street">street:</label>
+        <input
+          type="text"
+          name="street"
+          id="street"
+          value={street.value}
+          onChange={streetHandle}
+        ></input>
+      </div>
+      <div>
+        {" "}
+        <label htmlFor="postal-code">postal-code:</label>
+        <input
+          type="text"
+          name="postal-code"
+          id="postal-code"
+          value={postalCode.value}
+          onChange={postalCodeHandle}
+        ></input>
+      </div>
+      <div>
+        <label htmlFor="city">city:</label>
+        <input
+          type="text"
+          name="city"
+          id="city"
+          value={city.value}
+          onChange={cityHandle}
+        ></input>
+      </div>
+      <button type="submit">confirm</button>
+      <button type="button" onClick={props.onClose}>
+        cancel
+      </button>
+    </form>
+  );
+};
+
+export default CheckOut;
