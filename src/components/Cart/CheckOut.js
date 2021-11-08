1e551afee92d8b410004f9b0ff435e373e1f3498
Ahmed Mamdouh <tatah.douh.vom@gmail.com>
2021-11-08 14:44:21 +0200

    adding validating hook

--- src/components/Cart/CheckOut.js
+++ src/components/Cart/CheckOut.js
@@ -1,125 +1,64 @@
-import { useReducer } from "react";
-
 import classes from "./CheckOut.module.css";
-const initialValue = {
-  name: { value: "", isValid: false },
-  street: { value: "", isValid: false },
-  postalCode: { value: "", isValid: false },
-  city: { value: "", isValid: false },
+import useManagingInput from "../hooks/validate-input";
+
+const pcValid = (value) => {
+  const nomValue = Number(value);
+  return !isNaN(nomValue) && value.length === 4;
 };
-
-const reducerFunction = (state, action) => {
-  if (action.type === "STCHANGE") {
-    if (action.value.trim() !== "") {
-      return {
-        name: state.name,
-        street: { value: action.value, isValid: true },
-        postalCode: state.postalCode,
-        city: state.city,
-      };
-    } else {
-      return {
-        name: state.name,
-        street: { value: action.value, isValid: false },
-        postalCode: state.postalCode,
-        city: state.city,
-      };
-    }
-  }
-  if (action.type === "NACHANGE") {
-    if (action.value.trim() !== "") {
-      return {
-        name: { value: action.value, isValid: true },
-        street: state.street,
-        postalCode: state.postalCode,
-        city: state.city,
-      };
-    } else {
-      return {
-        name: { value: action.value, isValid: false },
-        street: state.street,
-        postalCode: state.postalCode,
-        city: state.city,
-      };
-    }
-  }
-  if (action.type === "CTCHANGE") {
-    if (action.value.trim() !== "") {
-      return {
-        name: state.name,
-        street: state.street,
-        postalCode: state.postalCode,
-        city: { value: action.value, isValid: true },
-      };
-    } else {
-      return {
-        name: state.name,
-        street: state.street,
-        postalCode: state.postalCode,
-        city: { value: action.value, isValid: false },
-      };
-    }
-  }
-  if (action.type === "PCCHANGE") {
-    const value = Number(action.value);
-    if (!isNaN(value) && action.value.length === 4) {
-      return {
-        name: state.name,
-        street: state.street,
-        postalCode: { value: action.value, isValid: true },
-        city: state.city,
-      };
-    } else {
-      return {
-        name: state.name,
-        street: state.street,
-        postalCode: { value: action.value, isValid: false },
-        city: state.city,
-      };
-    }
-  }
-
-  return initialValue;
-};
-
 
 const CheckOut = (props) => {
-    const [inputStates, dispatchInputStates] = useReducer(
-        reducerFunction,
-        initialValue
-        );
-        const { name, street, postalCode, city } = inputStates;
-        const submitHandle = (event) => {
-            event.preventDefault();
-            const userData = {
-                name: name.value,
-                street: street.value,
-                postalCode: postalCode.value,
-                city:city.value
-            }
-            props.onSubmit(userData)
-            
-        };
+  const {
+    value: nameValue,
+    isInputValid: isNameValid,
+    handleInput: handleName,
+  } = useManagingInput("", (value) => value.trim() !== "");
+  const {
+    value: streetValue,
+    isInputValid: isStreetValid,
+    handleInput: handleStreet,
+  } = useManagingInput("", (value) => value.trim() !== "");
+  const {
+    value: postalCodeValue,
+    isInputValid: isPostalCodeValid,
+    handleInput: handlePostalCode,
+  } = useManagingInput("", pcValid);
+  const {
+    value: cityValue,
+    isInputValid: isCityValid,
+    handleInput: handleCity,
+  } = useManagingInput("", (value) => value.trim() !== "");
+
+  const submitHandle = (event) => {
+    event.preventDefault();
+    const userData = {
+      name: nameValue,
+      street: streetValue,
+      postalCode: postalCodeValue,
+      city: cityValue,
+    };
+    props.onSubmit(userData);
+  };
 
   const nameHandle = (event) => {
-    dispatchInputStates({ type: "NACHANGE", value: event.target.value });
+    handleName(event.target.value);
   };
 
   const streetHandle = (event) => {
-    dispatchInputStates({ type: "STCHANGE", value: event.target.value });
+    handleStreet(event.target.value);
   };
 
   const postalCodeHandle = (event) => {
-    dispatchInputStates({ type: "PCCHANGE", value: event.target.value });
+    handlePostalCode(event.target.value);
   };
 
   const cityHandle = (event) => {
-    dispatchInputStates({ type: "CTCHANGE", value: event.target.value });
+    handleCity(event.target.value);
   };
-    
-    const submitability = name.isValid && street.isValid && postalCode.isValid && city.isValid
-    
-    console.log(submitability)
+
+  const submitability =
+    isNameValid && isStreetValid && isPostalCodeValid && isCityValid;
+  console.log(isNameValid);
+  console.log(submitability);
 
   return (
     <form className={classes.checkOut} onSubmit={submitHandle}>
@@ -130,7 +69,7 @@ const CheckOut = (props) => {
           type="text"
           name="name"
           id="name"
-          value={name.value}
+          value={nameValue}
           onChange={nameHandle}
         ></input>
       </div>
@@ -141,7 +80,7 @@ const CheckOut = (props) => {
           type="text"
           name="street"
           id="street"
-          value={street.value}
+          value={streetValue}
           onChange={streetHandle}
         ></input>
       </div>
@@ -152,7 +91,7 @@ const CheckOut = (props) => {
           type="text"
           name="postal-code"
           id="postal-code"
-          value={postalCode.value}
+          value={postalCodeValue}
           onChange={postalCodeHandle}
         ></input>
       </div>
@@ -162,12 +101,18 @@ const CheckOut = (props) => {
           type="text"
           name="city"
           id="city"
-          value={city.value}
+          value={cityValue}
           onChange={cityHandle}
         ></input>
       </div>
       <div className={classes.buttons}>
-        <button type="submit" className={classes.confirm} disabled={!submitability}>confirm</button>
+        <button
+          type="submit"
+          className={classes.confirm}
+          disabled={!submitability}
+        >
+          confirm
+        </button>
         <button type="button" onClick={props.onClose}>
           cancel
         </button>
